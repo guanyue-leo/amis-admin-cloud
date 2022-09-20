46221f94ec797bfa438ba071170b878b3f87771f
关跃 <c-guany01@vanke.com>
2022-09-20 22:36:31 +0800

    init for cloud

--- src/App.tsx
+++ src/App.tsx
@@ -8,144 +8,101 @@ import 'amis/lib/helper.css';
 import 'amis/sdk/iconfont.css';
 
 import axios from 'axios';
-import copy from 'copy-to-clipboard';
 
-import {render as renderAmis, ToastComponent, AlertComponent} from 'amis';
-import {alert, confirm, toast} from 'amis-ui';
+import {render as renderAmis, ToastComponent, AlertComponent, Button, AsideNav, Layout, confirm} from 'amis';
+import {alert, toast} from 'amis-ui';
 
 // amis 环境配置
-const env = {
-  // 下面三个接口必须实现
-  fetcher: ({
-    url, // 接口地址
-    method, // 请求方法 get、post、put、delete
-    data, // 请求数据
-    responseType,
-    config, // 其他配置
-    headers // 请求头
-  }: any) => {
-    config = config || {};
-    config.withCredentials = true;
-    responseType && (config.responseType = responseType);
-
-    if (config.cancelExecutor) {
-      config.cancelToken = new (axios as any).CancelToken(
-        config.cancelExecutor
-      );
-    }
-
-    config.headers = headers || {};
-
-    if (method !== 'post' && method !== 'put' && method !== 'patch') {
-      if (data) {
-        config.params = data;
+const myJson = {
+  type: 'page',
+  body: {
+    type: 'form',
+    api: '/api/form',
+    body: [
+      {
+        type: 'input-text',
+        name: 'name',
+        label: '姓名'
+      },
+      {
+        name: 'email',
+        type: 'input-email',
+        label: '邮箱'
+      },
+      {
+        name: 'color',
+        type: 'input-color',
+        label: 'color'
+      },
+      {
+        type: 'editor',
+        name: 'editor',
+        label: '编辑器'
       }
-      return (axios as any)[method](url, config);
-    } else if (data && data instanceof FormData) {
-      config.headers = config.headers || {};
-      config.headers['Content-Type'] = 'multipart/form-data';
-    } else if (
-      data &&
-      typeof data !== 'string' &&
-      !(data instanceof Blob) &&
-      !(data instanceof ArrayBuffer)
-    ) {
-      data = JSON.stringify(data);
-      config.headers = config.headers || {};
-      config.headers['Content-Type'] = 'application/json';
-    }
-
-    return (axios as any)[method](url, data, config);
-  },
-  isCancel: (value: any) => (axios as any).isCancel(value),
-  copy: (content: string) => {
-    copy(content);
-    toast.success('内容已复制到粘贴板');
+    ]
   }
-
-  // 后面这些接口可以不用实现
-
-  // 默认是地址跳转
-  // jumpTo: (
-  //   location: string /*目标地址*/,
-  //   action: any /* action对象*/
-  // ) => {
-  //   // 用来实现页面跳转, actionType:link、url 都会进来。
-  // },
-
-  // updateLocation: (
-  //   location: string /*目标地址*/,
-  //   replace: boolean /*是replace，还是push？*/
-  // ) => {
-  //   // 地址替换，跟 jumpTo 类似
-  // },
-
-  // isCurrentUrl: (
-  //   url: string /*url地址*/,
-  // ) => {
-  //   // 用来判断是否目标地址当前地址
-  // },
-
-  // notify: (
-  //   type: 'error' | 'success' /**/,
-  //   msg: string /*提示内容*/
-  // ) => {
-  //   toast[type]
-  //     ? toast[type](msg, type === 'error' ? '系统错误' : '系统消息')
-  //     : console.warn('[Notify]', type, msg);
-  // },
-  // alert,
-  // confirm,
 };
+const props = {};
+const env = {};
 
 class AMISComponent extends React.Component<any, any> {
   render() {
     return renderAmis(
-      // 这里是 amis 的 Json 配置。
-      {
-        type: 'page',
-        body: {
-          type: 'form',
-          api: '/api/form',
-          body: [
-            {
-              type: 'input-text',
-              name: 'name',
-              label: '姓名'
-            },
-            {
-              name: 'email',
-              type: 'input-email',
-              label: '邮箱'
-            },
-            {
-              name: 'color',
-              type: 'input-color',
-              label: 'color'
-            },
-            {
-              type: 'editor',
-              name: 'editor',
-              label: '编辑器'
-            }
-          ]
-        }
-      },
-      {
-        // props...
-      },
+      myJson,
+      props,
       env
     );
   }
+}
+
+function renderHeader() {
+  return (
+      <div>
+          <div className={`cxd-Layout-brandBar`}>
+              <div className={`cxd-Layout-brand`}>
+                  <i className="fa fa-paw"></i>
+                  <span className="hidden-folded m-l-sm">amis-admin-cloud</span>
+              </div>
+          </div>
+          <div className={`cxd-Layout-headerBar`}>
+              <div className="m-l-auto hidden-xs pull-right" style={{position: 'absolute', right: '30px', top: '50%', transform: 'translate(0, -50%)'}}>
+                  <span>admin</span>
+              </div>
+          </div>
+      </div>
+  );
+}
+
+function renderAside() {
+  const navigations = []
+
+  return (
+      <AsideNav
+          key={'folded-aside'}
+          navigations={[
+              {
+                  label: '',
+                  children: navigations
+              }
+          ]}
+      />
+  );
 }
 
 class APP extends React.Component<any, any> {
   render() {
     return (
       <>
-        <ToastComponent key="toast" position={'top-right'} />
-        <AlertComponent key="alert" />
-        <AMISComponent />
+        <Layout
+          aside={renderAside()}
+          header={renderHeader()}
+          folded={false}
+          offScreen={false}
+        >
+            <ToastComponent key="toast" position={'top-right'} />
+            <AlertComponent key="alert" />
+            <AMISComponent />
+        </Layout>
       </>
     );
   }
